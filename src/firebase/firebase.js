49eca3a6e5f3900fc5eae6af4fc12fca9f2873a1
Harshith <97115515+h4r5h1th@users.noreply.github.com>
2023-06-25 19:58:00 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTXq0ofPWoGUVOg_JHtA4S6OPIyKaR5fk",
-  authDomain: "netflix-clone-7401f.firebaseapp.com",
-  projectId: "netflix-clone-7401f",
-  storageBucket: "netflix-clone-7401f.appspot.com",
-  messagingSenderId: "76545067428",
-  appId: "1:76545067428:web:6c392c24dfc8ecb8c95931"
+  apiKey: "##################################",
+  authDomain: "##################################",
+  projectId: "##################################",
+  storageBucket: "##################################",
+  messagingSenderId: "##################################",
+  appId: "##################################"
 };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
